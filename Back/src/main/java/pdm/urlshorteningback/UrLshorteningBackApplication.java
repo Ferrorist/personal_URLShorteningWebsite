28ebacca934042a66c5e592d05189b6ae81e42f0
Ferrorist <ferrorist@naver.com>
2024-08-07 22:38:48 +0900

    init: Spring Boot Project

---
+++ Back/src/main/java/pdm/urlshorteningback/UrLshorteningBackApplication.java
@@ -0,0 +1,13 @@
+package pdm.urlshorteningback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLshorteningBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrLshorteningBackApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/pdm/urlshorteningback/UrLshorteningBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pdm.urlshorteningback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLshorteningBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
